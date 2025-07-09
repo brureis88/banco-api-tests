8bcc6133b296b9350a405b917fd7af5ce41cf1d6
brureis88 <brureis88@yahoo.com.br>
2025-07-09 13:28:13 -0300

    Adicionei testes para GET transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,16 +5,15 @@ const postTransferencias = require('../fixtures/postTransferencias.json')
 require('dotenv').config()
 
 describe('Transferências', () => {
+    let token
+
+    beforeEach(async () => {
+        token = await obterToken('julio.lima', '123456')
+    })
     describe('POST / transferencias', async () => {
-        let token
-
-        beforeEach(async () => {
-            token = await obterToken('julio.lima', '123456')
-        })
-
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
-            const bodyTransferencias = { ...postTransferencias}
-            
+            const bodyTransferencias = { ...postTransferencias }
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Contenct-Type', 'application/json')
@@ -24,15 +23,42 @@ describe('Transferências', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00', async () => {
-            const bodyTransferencias = { ...postTransferencias}
+            const bodyTransferencias = { ...postTransferencias }
             bodyTransferencias.valor = 7
-            
+
             const resposta = await request('http://localhost:3000')
                 .post('/transferencias')
                 .set('Contenct-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
                 .send(bodyTransferencias)
             expect(resposta.status).to.equal(422)
+        })
+    })
+
+    describe('GET / transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de transferencia contido no banco de dados quando o ID for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/10')
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(10)
+            expect(resposta.body.id).to.be.a('number')
+            expect(resposta.body.conta_origem_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(3)
+            expect(resposta.body.valor).to.equal(11.00)
+        })
+    })
+
+    describe('GET / transferencias/{id}', () => {
+        it('Deve retornar 10 elementos na paginacao quando informar limite de 10 registros', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.lengthOf(10)
         })
     })
 })
